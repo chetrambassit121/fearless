a1f5b63bd5fd9bcfcfc9276317796687e7723be3
Chetram Bassit <chetrambassit121@gmail.com>
2022-12-30 06:10:57 -0500

    .

--- snippets/views.py
+++ snippets/views.py
@@ -9,11 +9,13 @@ from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from rest_framework.reverse import reverse
 from rest_framework import renderers
-from rest_framework import viewsets
+from rest_framework import viewsets, serializers
 from rest_framework.decorators import action
 from rest_framework.response import Response
 from rest_framework import permissions
-
+from rest_framework.permissions import AllowAny
+from django.http import JsonResponse
+from rest_framework import status
 
 @api_view(['GET'])
 def api_root(request, format=None):
@@ -36,20 +38,41 @@ class SnippetViewSet(viewsets.ModelViewSet):
     This viewset automatically provides `list`, `create`, `retrieve`,
     `update` and `destroy` actions.
 
-    Additionally we also provide an extra `highlight` action.
+    Snippet List DELETE removes all snippets
+    Snippet Instance DELETE removes that specfic snippet
     """
     queryset = Snippet.objects.all()
     serializer_class = SnippetSerializer
-    permission_classes = [permissions.IsAuthenticatedOrReadOnly,
-                          IsOwnerOrReadOnly]
+    permission_classes = [AllowAny]
 
-    @action(detail=True, renderer_classes=[renderers.StaticHTMLRenderer])
-    def highlight(self, request, *args, **kwargs):
-        snippet = self.get_object()
-        return Response(snippet.highlighted)
+    def delete(self, request, format=None):
+        snippets = Snippet.objects.all()
+        if snippets:
+            snippets.delete()
+            return JsonResponse({"status":"ok"}, status=status.HTTP_200_OK)
+        return JsonResponse(serializers.errors, status=status.HTTP_400_BAD_REQUEST)
 
-    def perform_create(self, serializer):
-        serializer.save(owner=self.request.user)
+    # @action(detail=True, renderer_classes=[renderers.StaticHTMLRenderer])
+    # def highlight(self, request, *args, **kwargs):
+    #     snippet = self.get_object()
+    #     return Response(snippet.highlighted)
+
+    # def perform_create(self, serializer):
+    #     serializer.save(owner=self.request.user)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 # class UserList(generics.ListAPIView):
 #     queryset = User.objects.all()
